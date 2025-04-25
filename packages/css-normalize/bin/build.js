175bc0f9ac87a9a570a7307e3530cb7325d7e27b
Gajdascz <gajdascz@gmail.com>
2025-04-24 22:55:00 -0400

    chore: version packages

--- packages/css-normalize/bin/build.js
+++ packages/css-normalize/bin/build.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 const __dirname = import.meta.dirname;
-const srcPath = path.join(__dirname, '../src');
-const cssPath = path.join(__dirname, '../src/normalize.css');
-const buildPath = path.join(__dirname, '../build');
+const srcPath = path.join(__dirname, "../src");
+const cssPath = path.join(__dirname, "../src/normalize.css");
+const buildPath = path.join(__dirname, "../build");
 fs.mkdirSync(buildPath, { recursive: true });
 
-const css = fs.readFileSync(cssPath, 'utf-8').replace(/`/g, '\\`');
+const css = fs.readFileSync(cssPath, "utf-8").replace(/`/g, "\\`");
 
 fs.writeFileSync(`${buildPath}/string.js`, `export default \`${css}\``);
 
 fs.cpSync(srcPath, buildPath, { recursive: true });
 
-['scss', 'pcss', 'less', 'styl'].forEach((ext) => {
+["scss", "pcss", "less", "styl"].forEach((ext) => {
   fs.writeFileSync(`${buildPath}/normalize.${ext}`, css);
 });
